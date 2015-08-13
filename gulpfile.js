f753d40c3016b6e543346222a76db45062a48ab6
xiaomo <83387856@qq.com>
2015-08-13 11:40:06 +0800

    hero

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by IntelliJ IDEA.
- * User: xiaomo
- * Date: 15/8/13
- * Time: 10:23
- */
--- src/framework/angular2/angular2.dev.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by IntelliJ IDEA.
- * User: xiaomo
- * Date: 15/8/12
- * Time: 18:20
- */
